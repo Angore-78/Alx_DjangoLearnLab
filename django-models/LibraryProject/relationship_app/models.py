27a343f12a7d2127573bbebe23dcf740b8881fb9
Angore-78 <allanangore@gmail.com>
2025-09-06 08:36:42 +0300

    create working models in models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -8,20 +8,20 @@ class Author(models.Model):
 
 class Book(models.Model):
     title=models.CharField(max_length=30)
-    author=models.ForeignKey()
+    author=models.ForeignKey( Author,on_delete=models.CASCADE,related_name='book')
     def __str__(self):
         return self.title
     
 
 class Library(models.Model):
     name=models.CharField(max_length=40)
-    book=models.ManyToManyField(Book)
+    book=models.ManyToManyField(Book,related_name='library')
     def __str__(self):
         return self.name
 
 class Librarian(models.Model):
     name=models.CharField(max_length=50)
-    library=models.OneToOneField(Library)
+    library=models.OneToOneField(Library,on_delete=models.CASCADE,related_name='librarian')
     def __str__(self):
         return self.name
     
