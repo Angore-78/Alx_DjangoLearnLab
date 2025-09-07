89592f7db64edeb74ad1f98d213653cfedd482e2
Angore-78 <allanangore@gmail.com>
2025-09-07 07:05:12 +0300

    third correction to query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
-from .models import Author,Book,Library,Librarian
+from relationship_app.models import Author,Book,Library,Librarian
 
 def book_list(request):
     books = Book.objects.all()
-    author = books.author()
+    books.author()
 
 def library_catalogue(request):
     library_name=Library.objects.get(name=library_name)
-    available_books=library_name.book.all()
+    library_name.book.all()
 
 def specific_librarian(request):
     librarian_name = Librarian.objects.get(name=librarian_name)
-    library_posted = librarian_name.library() 
+    librarian_name.library() 
     
 
