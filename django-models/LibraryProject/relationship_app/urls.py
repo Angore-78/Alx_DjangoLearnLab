7eb2eed5388363d819f669e76adba26435db240e
Angore-78 <allanangore@gmail.com>
2025-09-07 10:59:30 +0300

    third correction of views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('books/',views.list_books,name='list_books'),
     path('library/',views.LibraryDetailView.as_view(),name='library'),
     path('register/', views.register,name = 'register'),
-    path('login/', LoginView.as_view(template_name = 'login.html'),name = 'login'),
-    path('logout', LogoutView.as_view(template_name='logout.html'), name = 'logout'),
+    path('login/', LoginView.as_view(template_name = ''),name = 'login'),
+    path('logout', LogoutView.as_view(template_name=''), name = 'logout'),
     ]
     
 
