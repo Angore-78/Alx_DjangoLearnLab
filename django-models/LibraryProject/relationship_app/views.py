5280e624af483027db594d2d9da34de07d1ab57d
Angore-78 <allanangore@gmail.com>
2025-09-07 08:15:20 +0300

    correction for task1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from .models import Book,Library
+from django.http import HttpResponse
 from django.views.generic import DetailView
 
 # Create your views here.
-def book_context_list(request):
-    book = Book.object.all()
-    book_display = {'catalogue':f'{book.title()} by {book.author()}'}
-    return render(request,'book/catalogue.html',book_display)
+def book_list(response):
+    books = Book.objects.get(title='',author='')
+    context = {'book_list':books}
+    return render(response,'relationship_app/list_books.html',context)
+                  
+class LibraryDetailView(DetailView):
+    model=Library
+    template_name='Library/library_detail.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        book = self.get_object()
+        context['books'] = book.book.all()
+        return context
 
-class LibraryDetailview(DetailView):
-    model = Library
-    template_name = 'library/library_detail.html'
-
-    def library_catalogue(self):
-        availed_books = self.object.book.all()
-        
+            
 
