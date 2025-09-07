2b84ec9520ce0e7238eee9011fe3ae31f1f02e15
Angore-78 <allanangore@gmail.com>
2025-09-07 06:41:30 +0300

    'add .gitignore'

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,5 +4,13 @@ from .models import Author,Book,Library,Librarian
 def book_list(request):
     books = Book.objects.all()
     author = books.author()
+
+def library_catalogue(request):
+    library_name=Library.objects.get(name=library_name)
+    available_books=library_name.book.all()
+
+def specific_librarian(request):
+    librarian_name = Librarian.objects.get(name=librarian_name)
+    library_posted = librarian_name.library() 
     
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Book,Library
+from django.views.generic import DetailView
 
 # Create your views here.
+def book_context_list(request):
+    book = Book.object.all()
+    book_display = {'catalogue':f'{book.title()} by {book.author()}'}
+    return render(request,'book/catalogue.html',book_display)
+
+class LibraryDetailview(DetailView):
+    model = Library
+    template_name = 'library/library_detail.html'
+
+    def library_catalogue(self):
+        availed_books = self.object.book.all()
+        
+
