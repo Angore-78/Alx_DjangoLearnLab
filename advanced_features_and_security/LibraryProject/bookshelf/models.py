91387c823aa2fd7a097243440699cbaf4322d1cc
Angore-78 <allanangore@gmail.com>
2025-09-13 08:41:39 +0300

    'create custom user model'

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Book(models.Model):
@@ -8,3 +9,8 @@ class Book(models.Model):
 
     def __str__(self):
         return f'{self.title} by {self.author}'
+    
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateTimeField()
+    profile_photo = models.ImageField()
+    
