e85cd4ffcd4738531ed8530ca82afbb153d2a8a0
Angore-78 <allan.angore@gmail.com>
2025-09-13 06:29:13 +0000

    adjustment to custom user manager

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -14,7 +14,7 @@ class CustomUser(AbstractUser):
     date_of_birth = models.DateTimeField()
     profile_photo = models.ImageField()
    
-classmodels.CustomUserManager: 
+classmodels.CustomUserManager(BaseUserManager): 
     def create_user(self,date_of_birth,profile_photo):
         CustomUser.objects.create(date_of_birth=date_of_birth,profile_photo=profile_photo )
     def create_superuser(self,date_of_birth,profile_photo):
