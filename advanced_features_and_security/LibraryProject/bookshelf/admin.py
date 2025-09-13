fd727822857982e0c638639b903dda7ef2d446bc
Angore-78 <allan.angore@gmail.com>
2025-09-13 06:44:18 +0000

    configuration to admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 
 # Register your models here.
+admin.site.register(CustomUser,UserAdmin)
