d437deb4748c017f962749ac0bef38f8fe43d82f
Angore-78 <allanangore@gmail.com>
2025-09-13 22:47:11 +0300

    'add book_list parameter to permission_granter function'

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -7,4 +7,5 @@ from django.contrib.auth.decorators import permission_required
 @permission_required('bookshelf.can_edit',raise_exception = True)
 @permission_required('bookshelf.can_delete')
 @permission_required('bookshelf.can_view',raise_exception = True)
-def permission_granter(request)
+def permission_granter(request,book_list):
+    pass
