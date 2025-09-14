88665c3515b985e860d1030e1b80f31fb5ea9e97
Angore-78 <allan.angore@gmail.com>
2025-09-14 05:37:49 +0000

    'modified views.py'

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,7 +3,14 @@ from django.contrib.auth.decorators import permission_required
 from .forms import ExampleForm
 # Create your views here.
 <form method = 'post'>{%csrf_token%} 
+def form_example(request):
+    return HttpResponse(response 'form_example.html')
 
+
+def book_views(request):
+    return HttpResponse(response,'book_list.html')
+
+    
 @permission_required('bookshelf.can_create',raise_exception = True)
 @permission_required('bookshelf.can_edit',raise_exception = True)
 @permission_required('bookshelf.can_delete')
