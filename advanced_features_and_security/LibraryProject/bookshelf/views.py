233a6fbaeee8d0ef6112d49e7a40170435478e13
Angore-78 <allan.angore@gmail.com>
2025-09-14 05:27:29 +0000

    import ExampleForm into views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
-
+from .forms import ExampleForm
 # Create your views here.
+<form method = 'post'>{%csrf_token%} 
 
 @permission_required('bookshelf.can_create',raise_exception = True)
 @permission_required('bookshelf.can_edit',raise_exception = True)
