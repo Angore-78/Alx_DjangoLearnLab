dcbf8e1af5397014d7fcda71028bcd698456f776
Angore-78 <allanangore@gmail.com>
2025-09-26 21:59:43 +0300

    'Correcting typo in urls.py'

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,8 +6,8 @@ from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
 url_patterns = [
     path('api/books/',ListView.as_view(),name = 'books'),
     path('api/books/<int:pk>/', DetailView.as_view(), name = 'books/<int:pk>/'),
-    path('api/book/create/', CreateView.as_view(), name = 'create'),
-    path('api/book/update', UpdateView.as_view(), name = 'update' ),
-    path('api/')
+    path('api/books/create/', CreateView.as_view(), name = 'create'),
+    path('api/books/update', UpdateView.as_view(), name = 'update' ),
+    path('api/books/delete', DeleteView.as_view(),name = 'delete')
 ]
 
