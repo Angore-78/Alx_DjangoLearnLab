d36ffe9715a4b9d0e186dbe2511285ed48733891
Angore-78 <allanangore@gmail.com>
2025-09-26 21:56:55 +0300

    'configure urls for new APIViewsets in views.py'

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import 
+from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+
 
 url_patterns = [
-    
+    path('api/books/',ListView.as_view(),name = 'books'),
+    path('api/books/<int:pk>/', DetailView.as_view(), name = 'books/<int:pk>/'),
+    path('api/book/create/', CreateView.as_view(), name = 'create'),
+    path('api/book/update', UpdateView.as_view(), name = 'update' ),
+    path('api/')
 ]
 
