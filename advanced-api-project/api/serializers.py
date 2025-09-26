6c8b79e8b3cd8fee1cbea143b338a22e1f14d45e
Angore-78 <allanangore@gmail.com>
2025-09-26 18:21:56 +0300

    name change from serializer.py to serializers.py and add custom validation for BookSerializer

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 from .models import Author,Book
-
+from django.utils import timezone
 #Modelserializer for Book model,inherits from serializers.ModelSerializer
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        field = ['id','title','author',]
+        field = ['id','title','author','publication year']
+    
+    def validate(self,publication_year):
+        if publication_year >timezone.now:
+            raise serializers.ValidationError("A book's publication date can't be a time beyond now.")
+        return publication_year
+
 
 #Modelserializer for Author model,inherits from serializers.ModelSerializer
 class AuthorSerializer(serializers.ModelSerializer):
