20e56bf446ac0983cec4f40e0881085bcb834441
Angore-78 <allanangore@gmail.com>
2025-09-26 22:26:29 +0300

    'added third correction to views.py'

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Book
-from rest_framework.permissions import IsAuthenticatedOrReadOnly,IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import generics 
 from .serializers import BookSerializer
 
