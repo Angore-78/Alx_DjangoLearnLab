187a1bb50262807edb7c44634587911f069d7741
Angore-78 <allanangore@gmail.com>
2025-09-26 18:39:02 +0300

    configure serializers to suite the many to many relationship between Book model and Author model

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
 from .models import Author,Book
 from django.utils import timezone
+
+
+#Modelserializer for Author model,inherits from serializers.ModelSerializer
+class AuthorSerializer(serializers.ModelSerializer):
+    author_name = serializers.CharField(source = 'author.name',read_inly = True)
+    class Meta:
+        model = Author
+        fields = ['id','name',]
+
+
+
 #Modelserializer for Book model,inherits from serializers.ModelSerializer
 class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(many = True, read_only = True)
     class Meta:
         model = Book
         field = ['id','title','author','publication year']
@@ -13,8 +25,4 @@ class BookSerializer(serializers.ModelSerializer):
         return publication_year
 
 
-#Modelserializer for Author model,inherits from serializers.ModelSerializer
-class AuthorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = ['id','name',]
+
