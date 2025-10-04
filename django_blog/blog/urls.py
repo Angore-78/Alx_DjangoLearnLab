0d507f8f10234b72a191e5937973a578d4aefc5e
Angore-78 <allanangore@gmail.com>
2025-10-04 16:49:35 +0300

    configured url paths for generic views created in views.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import BlogListView
+from . import views
 
 app_name = 'blog'
 
 url_patterns=[
-    path('post/',BlogListView.as_view(),name='blog'),
-    
+    path('post/',views.BlogListView.as_view(),name='blog'),
+    path('post/<int:pk>/delete',views.DeleteView.as_view(),name='delete'),
+    path('post/<int:pk>/update/',views.UpdateView.as_view,name='update'),
+    path('post/<int:pk>/detail/',views.BlogDetailView.as_view(),name='detail'),
+    path('post/new/',views.BlogCreateView.as_view(),name='post')
 ]
