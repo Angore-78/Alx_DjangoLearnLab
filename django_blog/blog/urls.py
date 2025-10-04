e5d63e058cbce87e41ff5c5e97b98204b137a78c
Angore-78 <allanangore@gmail.com>
2025-10-04 16:53:30 +0300

    forgot closing forward slash in DeleteView  url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,7 +6,7 @@ app_name = 'blog'
 
 url_patterns=[
     path('post/',views.BlogListView.as_view(),name='blog'),
-    path('post/<int:pk>/delete',views.DeleteView.as_view(),name='delete'),
+    path('post/<int:pk>/delete/',views.DeleteView.as_view(),name='delete'),
     path('post/<int:pk>/update/',views.UpdateView.as_view,name='update'),
     path('post/<int:pk>/detail/',views.BlogDetailView.as_view(),name='detail'),
     path('post/new/',views.BlogCreateView.as_view(),name='post')
