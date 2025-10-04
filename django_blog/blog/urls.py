2dbdc82809e95734d837a7baeedb16957e52ed03
Angore-78 <allanangore@gmail.com>
2025-10-04 16:44:54 +0300

    configured url paths for generic views created in views.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import BlogListView
+from . import views
 
 app_name = 'blog'
 
 url_patterns=[
-    path('post/',BlogListView.as_view(),name='blog'),
-    
+    path('post/',views.BlogListView.as_view(),name='blog'),
+    path('post/<int:pk>/delete',views.Delete.as_view(),name='delete'),
+    path('post/<int:pk>/update/',views.UpdateView.as_view,name='update'),
+    path('post/new/',views.BlogCreateView.as_view,name='post')
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 from .models import Post
 from django.views.generic import ListView,DeleteView,CreateView,UpdateView,DetailView
+
+def index(request):
+    return render(request,'blog/index.html')
 
 
 class BlogCreateView(CreateView):
     blog_post=Post.objects.create()
-    template_name='new_post.html'
+    template_name='post.html'
 
 
 class BlogListView(ListView,id):
     blog_posts=Post.objects.order_by("published_date",id)
-    template_name='blog/blog.html'
+    template_name='list/.html'
     
 class BlogUpdateView(UpdateView):
     blog_post=Post.objects.update()
@@ -19,3 +23,7 @@ class BlogUpdateView(UpdateView):
 class BlogDetailView(DetailView):
     blog_post=Post.objects.all()
     template_name='detail.html'
+    
+
+class BlogDeleteView(DeleteView):
+    template_name='delete.html'
