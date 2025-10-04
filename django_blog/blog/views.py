67d3811b548c7a722440b4a6a8be6cd37ae33495
Angore-78 <allanangore@gmail.com>
2025-10-04 15:44:13 +0300

    add templates/blog for templates base.html, login.html and logout.html

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView,DeleteView,CreateView,UpdateView,DeleteView
+from django.views.generic import ListView,DeleteView,CreateView,UpdateView,DeleteView,DetailView
 
 
