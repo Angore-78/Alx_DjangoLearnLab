a622243a0edd124ab5e351fba43add16df09a544
Angore-78 <allanangore@gmail.com>
2025-10-04 16:31:22 +0300

    make database migrations, create generic class views that perform CRUD operations

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView,DeleteView,CreateView,UpdateView,DeleteView,DetailView
+from django.views.generic import ListView,DeleteView,CreateView,UpdateView,DetailView
 
 
-class BlogListView(ListView):
-    blog_posts=Post.objects.order_by("published_date")
+class BlogCreateView(CreateView):
+    blog_post=Post.objects.create()
+    template_name='new_post.html'
+
+
+class BlogListView(ListView,id):
+    blog_posts=Post.objects.order_by("published_date",id)
     template_name='blog/blog.html'
     
+class BlogUpdateView(UpdateView):
+    blog_post=Post.objects.update()
+    template_name='edit.html'
+
+class BlogDetailView(DetailView):
+    blog_post=Post.objects.all()
+    template_name='detail.html'
