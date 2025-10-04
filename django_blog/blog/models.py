072bf4b97e03c71bcbdde15963c95642d025a923
Angore-78 <allanangore@gmail.com>
2025-10-04 12:01:30 +0300

    create CustomUser model inheriting from AbstractBaseUser

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,8 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser,User
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author=models.ForeignKey(User,on_delete=models.CASCADE)
+
+class CustomUser(AbstractBaseUser):
+    username = models.CharField(max_length=50,unique=True)
+    email=models.EmailField(max_length=100,unique=True)
+    sign_up_date=models.DateTimeField(auto_created=True)
+    
+    REQUIRED_FIELDS=['email']
+
+    def __str__(self):
+        return f"{self.username}, you have successfully loged in."
