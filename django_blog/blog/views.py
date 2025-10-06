d07ad22f7e8d6ff991fdba74da2ab7946be7a812
Angore-78 <allanangore@gmail.com>
2025-10-06 22:01:35 +0300

    Datebaseconfiguration swith to PostgreSQL, import User in views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-from django.
+from django.contrib.auth.models import User
 from .models import Post
 from django.urls import reverse_lazy
 from django.views.generic import ListView,DeleteView,CreateView,UpdateView,DetailView
