1507f8ccf5183b1277c9a51eb2207e84cf81baeb
Angore-78 <allanangore@gmail.com>
2025-10-10 21:14:31 +0300

    correction to import of AbstractUser class in accounts/models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import UserManager,AbstractUser,PermissionsMixin
+from django.contrib.auth.models import AbstractUser
 from posts.models import Post
 
 
