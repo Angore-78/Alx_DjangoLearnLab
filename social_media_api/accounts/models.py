269e728c1e7e749107f603c012b43036083355ec
Angore-78 <allanangore@gmail.com>
2025-10-10 15:51:42 +0300

    create models and makemigrations

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class CustomUser(AbstractBaseUser):
+    bio=models.TextField()
+    profile_picture=models.ImageField()
+    followers=models.ManyToManyField(to='CustomUser')
