8852a102a2a3efa949b4c047cb406d4ba99ff7a2
Angore-78 <allanangore@gmail.com>
2025-10-12 17:58:50 +0300

    correct import of IsAuthenticated to import for permissions

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,12 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 from .serializers import UserRegistrationSerializer,LoginSerializer,UserProfileSerializer
 from .models import CustomUser
 from rest_framework.views import APIView
 from rest_framework import generics
 
-class RegistrationView(generics.CreateAPIView):
+class RegistrationView(generics.GenericAPIView):
     queryset=CustomUser.objects.create_user()
     serializer_class=UserRegistrationSerializer
 
@@ -19,12 +19,12 @@ class RegistrationView(generics.CreateAPIView):
 
 
 class UserProfile(generics.ModelViewSet):
-    querry_set=CustomUser.objects.get('profile')
+    querry_set=CustomUser.objects.all().get('profile')
     serializer_class=UserProfileSerializer
-    permission_classes=[IsAuthenticated]
+    permission_classes=[permissions.IsAuthenticated]
 
 
-class UserLoginView(APIView):
+class UserLoginView(generics.GenericAPIView):
     def post(self,request):
         serializer=LoginSerializer(data=request.data,context={'request':request})
         serializer.is_valid(raise_exception=True)
