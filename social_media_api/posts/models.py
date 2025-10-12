34d866f1d2eb9d9c9a59d6f14d7070deb126f8ca
Angore-78 <allanangore@gmail.com>
2025-10-12 09:29:30 +0300

    correct User to user in posts/models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from accounts.models import CustomUser as user
 
 class Post(models.Model):
-    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    author=models.ForeignKey(user,on_delete=models.CASCADE)
     title=models.TextField()
     content=models.CharField(max_length=250)
     created_at=models.DateTimeField(auto_now_add=True)
