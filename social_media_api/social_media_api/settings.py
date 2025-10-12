f29e7b792dddcb16ceeb0509b452471352e2295a
Angore-78 <allanangore@gmail.com>
2025-10-12 18:16:57 +0300

    configurations for deployment

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+PORT='8000'
+USER='LOCAL HOST'
+
 import os
 import secrets
 from pathlib import Path
