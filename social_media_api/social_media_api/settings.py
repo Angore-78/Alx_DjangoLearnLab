c324e9a9bcded390d20b107348fc76f10d4f6170
Angore-78 <allanangore@gmail.com>
2025-10-12 15:37:12 +0300

    configurations for deployment in social_media_api/settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_BROWSER_XSS_FILTER=True
+X_FRAME_OPTIONS=True
+SECURE_SSL_REDIRECT=True
