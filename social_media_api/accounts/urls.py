3b9291a301fd65c840b94d1e643bf4f9a5a8daa1
Angore-78 <allanangore@gmail.com>
2025-10-12 18:09:25 +0300

    proper urls in accounts/urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name='accounts'
 
 urlpatterns=[
-    path('/register',views.RegistrationView.as_view()),
-    path('/login',views.UserLoginView.as_view()),
-    path('/profile',views.UserProfile.as_view()),
+    path('/register/',views.RegistrationView.as_view()),
+    path('/login/',views.UserLoginView.as_view()),
+    path('/profile/',views.UserProfile.as_view()),
 ]
 
 
